5dde14e28f7c6d68801c647a06202a1ceaffbd2f
kenji123 <jeanstarwin@gmail.com>
2015-04-25 23:36:07 -0400

    added license header to libstalkerclient. resolves #4

--- src/libstalkerclient/itv.h
+++ src/libstalkerclient/itv.h
@@ -1,8 +1,23 @@
-/* 
- * File:   itv.h
- * Author: kenji123
+/*
+ *      Copyright (C) 2015  Jamal Edey
+ *      http://www.kenshisoft.com/
  *
- * Created on April 7, 2015, 4:08 PM
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Kodi; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
  */
 
 #ifndef ITV_H
