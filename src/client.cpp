01efeff4cd77eef2fe390d4591433e2520f51f5b
Alwin Esch <alwin.esch@web.de>
2017-04-26 16:37:57 +0200

    Remove not used part

--- src/client.cpp
+++ src/client.cpp
@@ -175,19 +175,8 @@ ADDON_STATUS ADDON_GetStatus() {
     return m_currentStatus;
 }
 
-bool ADDON_HasSettings() {
-    return true;
-}
-
-unsigned int ADDON_GetSettings(ADDON_StructSetting ***sSet) {
-    return 0;
-}
-
 ADDON_STATUS ADDON_SetSetting(const char *settingName, const void *settingValue) {
     return ADDON_STATUS_NEED_RESTART;
-}
-
-void ADDON_FreeSettings() {
 }
 
 /***********************************************************
