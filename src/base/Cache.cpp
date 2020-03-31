ae06e49e7e15bde0603ca46c65a53a46bc0da24c
Alwin Esch <alwin.esch@web.de>
2020-03-31 10:53:57 +0200

    update source license to use SPDX

--- src/base/Cache.cpp
+++ src/base/Cache.cpp
@@ -1,22 +1,9 @@
 /*
- *      Copyright (C) 2016  Jamal Edey
+ *  Copyright (C) 2015-2020 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2016 Jamal Edey
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Kodi; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- *  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
  */
 
 #include "Cache.h"
